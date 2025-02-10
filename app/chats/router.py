0439f3e0ce4d66d40a146757cd34bc7c581bd277
Mikita <Nikikiki01102005@gmail.com>
2025-02-10 21:18:11 +0300

    start chat model

---
+++ app/chats/router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, WebSocket
+
+from chats.service import manager
+
+router = APIRouter(
+    prefix="/chat",
+    tags=["chat"],
+)
+
+
+@router.websocket("/{chat_id}")
+async def websocket_endpoint(websocket: WebSocket, chat_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client {chat_id}: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
